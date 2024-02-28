2caeb843cd2f4111db66e4760c541f3413e75ed8
jathinreddy4 <k.jathinreddy452@gmail.com>
2024-02-29 00:11:12 +0530

    .

--- rotateMatrix.java
+++ rotateMatrix.java
@@ -27,7 +27,29 @@ public class rotateMatrix
                 ans[j][n-1-i] = arr[i][j];
             }
         }
-        return ans;
+        
+        for(int i=0; i<ans.length;i++)
+        {
+            for(int j=0; j<ans.length;j++)
+            {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        
+        int k = ans.length;
+        int answ[][] = new int[k][k];
+
+        for(int i=0; i<ans.length; i++)
+        {
+            for(int j=0; j<ans.length;j++)
+            {
+                answ[j][k-1-i] = arr[i][j];
+            }
+        }
+        
+        return answ;
     }
 
 }
